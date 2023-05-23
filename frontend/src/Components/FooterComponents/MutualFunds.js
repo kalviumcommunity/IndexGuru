e012f230f95cdfbdf3b40ff095e275293c5105db
AnjaniRai <hritek001@gmail.com>
2023-05-23 10:24:44 +0530

    Updated the styling of footer

---
+++ frontend/src/Components/FooterComponents/MutualFunds.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import "./MutualFund.css"
+
+
+function MutualFunds() {
+
+
+
+  return (
+    <>
+    
+    <div className='div1'>
+    <div className='iframe'>
+<iframe width="560" 
+height="415" 
+src="https://www.youtube.com/embed/5E_LZoAGmwM"
+ title="YouTube video player"
+ frameborder="0"
+ allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+allowfullscreen></iframe>
+
+</div>
+
+
+
+       <div className='description' >
+       
+        <h2 className='Simplifying'>
+        Mutual Funds: Simplifying the Basics
+        </h2>
+      
+     
+       
+      
+        <h4 style={{textAlign:"center", marginBottom: "1.5em", color: "#10b983"} }>
+        Understanding the Concept
+        </h4>
+
+        To many people, Mutual Funds can seem complicated or intimidating. We are going to try and simplify it for you at its very basic level.
+        <br />
+        <br />
+        Mutual funds are investment vehicles that offer individuals a convenient and accessible way to<br /> participate in the financial markets. They provide an opportunity to invest in a diversified <br /> portfolio of securities without the need for extensive knowledge or experience in investing.<br /> Whether you are a beginner or an experienced investor, mutual funds can serve as an excellent tool <br /> to achieve your financial goals.
+        <br />
+        <br />
+        At its core, a mutual fund is a pool of money contributed by a large number of individuals, known as investors<br />. These investors share a common investment objective, such as capital appreciation, income generation<br /> or both. By pooling their resources together, they can access a professionally managed portfolio<br /> of investments that offers diversification across various asset classes.
+        <br />
+        <br />
+        The fund is managed by a professional fund manager or a team of experts who specialize in analyzing <br /> and selecting suitable investment options. These professionals conduct thorough research, monitor <br />market trends, and make informed decisions on behalf of the investors. Their expertise helps to navigate <br /> the complexities of the financial markets and optimize the fund's performance.
+        <br />
+        <br />
+        Investing in a mutual fund offers several advantages. First and foremost, it allows individuals to benefit<br /> from diversification. By investing in a mutual fund, you gain exposure to a broad range of securities <br /> , such as stocks, bonds, and money market instruments. This diversification helps to spread risk and<br /> potentially enhance returns.
+        <br />
+        <br />
+        Furthermore, mutual funds provide access to investment opportunities that may be otherwise inaccessible<br /> or impractical for individual investors. The collective buying power of the fund allows for investments in a wide<br /> range of companies, industries, and geographic regions. This diversification across different securities and markets <br />can help to mitigate the impact of market fluctuations and reduce the risk associated with individual investments.
+        <br />
+        <br />
+
+</div>
+
+
+
+</div>
+
+<div className='points'>
+
+  
+</div>
+
+    </>
+);
+}
+export default MutualFunds;
