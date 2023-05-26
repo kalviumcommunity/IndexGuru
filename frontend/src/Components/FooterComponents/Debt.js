9df5150ac313a498f28e49adb07bcda737ee5668
AnjaniRai <hritek001@gmail.com>
2023-05-26 15:54:04 +0530

    Added Debt

--- frontend/src/Components/FooterComponents/Debt.js
+++ frontend/src/Components/FooterComponents/Debt.js
@@ -11,12 +11,11 @@ function Debt() {
     
     <div className='div69'>
     <div className='iframe'>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/IeRSr6F2NOM" title="YouTube video player"
-     frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-     allowfullscreen>
-
-     </iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/BZPp-lXcN4o" 
+    title="YouTube video player"
+     frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    allowfullscreen></iframe>
 <div class="credit-container">
 <p class="credit-text">
       Powered by <a class="credit-link" href="https://www.mutualfundssahihai.com/en/what-is-a-mutual-fund">Mutual Fund Sahi Hai</a></p>
@@ -28,34 +27,30 @@ function Debt() {
        <div className='description' >
        
         <h2 className='Simplifying'>
-       What are Equity Funds?
+        What are Debt Funds?
         </h2>
       
      
 
-        An Equity Fund is a Mutual Fund Scheme that invests predominantly in shares/stocks of companies. They are also known as Growth Funds.
+        A debt fund is a Mutual Fund scheme that invests in fixed income instruments, such as Corporate and Government Bonds, corporate debt securities, and money market instruments etc. that offer capital appreciation. Debt funds are also referred to as Fixed Income Funds or Bond Funds.
         <br />
         <br />
-        Equity Funds are either Active or Passive.  In an Active Fund, a fund manager scans the market, conducts research on companies, examines performance and looks for the best stocks to invest.
+        A few major advantages of investing in debt funds are low cost structure, relatively stable returns, relatively high liquidity and reasonable safety.
         <br />
         <br />
-        In a Passive Fund, the fund manager builds a portfolio that mirrors a popular market index, say Sensex or Nifty Fifty.
+        Debt funds are ideal for investors who aim for regular income, but are risk-averse. Debt funds are less volatile and, hence, are less risky than equity funds. If you have been saving in traditional fixed income products like Bank Deposits, and looking for steady returns with low volatility, debt Mutual Funds could be a better option, as they help you achieve your financial goals in a more tax efficient manner and therefore earn better returns.
         <br />
         <br/>
-        Furthermore, Equity Funds can also be divided as per Market Capitalisation, i.e. how much the capital market values an entire company’s equity. There can be Large Cap, Mid Cap, Small or Micro Cap Funds.
+        In terms of operation, debt funds are not entirely different from other Mutual Fund schemes. However, in terms of safety of capital, they score higher than equity Mutual Funds.
         <br />
         <br />
-        Also there can be a further classification as Diversified or Sectoral / Thematic. In the former, the scheme invests in stocks across the entire market spectrum, while in the latter it is restricted to only a particular sector or theme, say, Infotech or Infrastructure.
-        <br />
-        <br />
-        Thus, an equity fund essentially invests in company shares, and aims to provide the benefit of professional management and diversification to ordinary investors.
-        <br />
-        <br />
-
-        Equity funds can be categorized into two main types: active funds and passive funds. In an active fund, a skilled fund manager actively manages the fund by conducting market research, analyzing company performance, and selecting stocks that are expected to outperform the market. The fund manager's expertise and research play a crucial role in the fund's performance.
-        <br />
-        <br />
-        On the other hand, passive funds, also known as index funds, aim to replicate the performance of a specific market index, such as the Sensex or Nifty Fifty. The fund manager builds a portfolio that mirrors the composition of the chosen index. Passive funds generally have lower expense ratios compared to active funds, as they require less active management.
+        Debt funds offer several advantages that make them attractive to investors. Firstly, they typically have a lower cost structure compared to other investment options. The expense ratios of debt funds tend to be relatively lower, which means a larger portion of the investor's returns is retained.
+        <br/>
+        <br/>
+        Secondly, debt funds aim to provide relatively stable returns compared to equity funds. The underlying fixed income instruments generate income through regular interest payments, providing investors with a predictable stream of income. While there may be fluctuations in interest rates and bond prices, the impact on debt fund returns is generally lower compared to the volatility experienced in equity markets.
+        <br/>
+        <br/>
+        Overall, debt funds provide investors with the opportunity to earn steady returns, benefit from tax efficiency, and diversify their investment portfolios beyond traditional fixed income products. However, it's important to conduct thorough research, evaluate the fund's investment strategy, and consult with a financial advisor to determine if debt funds align with your investment goals and risk appetite.
 
 </div>
 
